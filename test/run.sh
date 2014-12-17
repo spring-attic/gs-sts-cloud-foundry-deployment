fdcae022302a2eec9e3d51c54326d4bc834b740e
Roy Clarkson <rclarkson@pivotal.io>
2014-12-16 18:52:58 -0600

    Add CI script

---
+++ test/run.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+cd $(dirname $0)
+
+cd ../sample
+
+mvn clean package
+ret=$?
+if [ $ret -ne 0 ]; then
+  exit $ret
+fi
+rm -rf target
+
+./gradlew build
+ret=$?
+if [ $ret -ne 0 ]; then
+  exit $ret
+fi
+rm -rf build
+
+exit
