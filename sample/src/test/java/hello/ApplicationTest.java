2eb763b30c69f60f26a76789b904f0284cfdc68a
Greg Turnquist <gturnquist@pivotal.io>
2017-06-06 15:47:25 -0500

    Upgrade to Spring Boot 1.5.3

---
+++ sample/src/test/java/hello/ApplicationTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2012-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package hello;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class ApplicationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @Test
+    public void greeting() throws Exception {
+        mockMvc.perform(get("/greeting"))
+                .andExpect(content().string(containsString("Hello, World!")));
+    }
+
+    @Test
+    public void greetingWithUser() throws Exception {
+        mockMvc.perform(get("/greeting").param("name", "SomeUser"))
+                .andExpect(content().string(containsString("Hello, SomeUser!")));
+    }
+
+}
